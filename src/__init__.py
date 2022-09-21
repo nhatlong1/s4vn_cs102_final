a32a33b82cfb9d539dfadfc86219bc7ea0ac04a9
nhatlong1 <phanmainhatlong@outlook.com>
2022-09-21 15:23:07 +0700

    # Remove Everything
    Remove everything and start over

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-# Importing the classes and functions from the other files.
-from projectile.includes.camera import Camera
-from projectile.includes.function import after, blur_screen, get_offset, pg_coord, pm_coord
-from projectile.includes.selector import ObjectSelector
-from projectile.includes.lx_sprites import Projectile, Boundary, StaticObstacle
-from projectile.includes.constants import *
